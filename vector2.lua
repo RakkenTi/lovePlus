930d76649904e16a7ce0a008e025b69ab1f75cb9
RakkenTi <martin3zmrk@gmail.com>
2025-04-04 09:48:09 -0400

    added type

--- vector2.lua
+++ vector2.lua
@@ -1,6 +1,6 @@
 --- @class Vector2dClass
-local vector2d = {}
-vector2d.__index = vector2d
+local vector2 = {}
+vector2.__index = vector2
 
 --- @class Vector2d
 --- Creates a new Vector2d object.
@@ -9,9 +9,9 @@ vector2d.__index = vector2d
 --- @param x number
 --- @param y number
 --- @return Vector2d
-function vector2d.new(x, y)
+function vector2.new(x, y)
     local self = setmetatable({
-    }, vector2d)
+    }, vector2)
 
     self.x = x or 0
     self.y = y or 0
@@ -20,41 +20,41 @@ function vector2d.new(x, y)
 end
 
 --// Metamethods
-function vector2d.__index(t, k)
+function vector2.__index(t, k)
 
     if (k == "Magnitude") then
         return math.sqrt(t.x ^ 2 + t.y ^ 2)
     end
 
     if (k == "Unit") then
-        return vector2d.new(t.x / t.Magnitude, t.y / t.Magnitude)
+        return vector2.new(t.x / t.Magnitude, t.y / t.Magnitude)
     end
 
     return rawget(t, k)
 end
 
-function vector2d.__add(a, b)
-    return vector2d.new(a.x + b.x, a.y + b.y)
+function vector2.__add(a, b)
+    return vector2.new(a.x + b.x, a.y + b.y)
 end
 
-function vector2d.__sub(a, b)
-    return vector2d.new(a.x - b.x, a.y - b.y)
+function vector2.__sub(a, b)
+    return vector2.new(a.x - b.x, a.y - b.y)
 end
 
-function vector2d.__div(a, b)
-    return vector2d.new(a.x / b, a.y / b)
+function vector2.__div(a, b)
+    return vector2.new(a.x / b, a.y / b)
 end
 
-function vector2d.__mul(a, b)
-    return vector2d.new(a.x * b, a.y * b)
+function vector2.__mul(a, b)
+    return vector2.new(a.x * b, a.y * b)
 end
 
-function vector2d.__eq(a, b)
+function vector2.__eq(a, b)
     return a.x == b.x and a.y == b.y
 end
 
-function vector2d.__tostring(a)
+function vector2.__tostring(a)
     return "("..a.x..","..a.y..")"
 end
 
-return vector2d
+return vector2
