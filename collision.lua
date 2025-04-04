deedaeeb1585ba5915d4b20687b4ff5ee393a6eb
RakkenTi <martin3zmrk@gmail.com>
2025-04-04 11:16:35 -0400

    Updated collision.lua checkRect()

--- collision.lua
+++ collision.lua
@@ -4,10 +4,17 @@ local collision = {}
 --- Checks if a given position is within a rectangle defined by a position and size.
 --- Assumes that (0,0) is the top-left.
 --- Also assumes that all dimensions are in pixels.
+--- Make sure to give a
+--- @param targetPosX number The x-position of the point that will be checked.
+--- @param targetPosY number The y-position of the point that will be checked.
+--- @param rootPosX number The x-position of the rectangle assuming an anchor of (0,0)
+--- @param rootPosY number the y-position of the rectangle assuming an anchor of (0, 0)
+--- @param rectSizeX number The width of the rectangle.
+--- @param rectSizeY number The height of the rectangle.
 --- @return boolean
-function collision.checkRect(targetPosX, targetPosY, objectPosX, objectPosY, xSize, ySize)
-    local isXBound = targetPosX > objectPosX and targetPosX < (objectPosX + xSize)
-    local isYBound = targetPosY > objectPosY and targetPosY < (objectPosY + ySize)
+function collision.checkRect(targetPosX, targetPosY, rootPosX, rootPosY, rectSizeX, rectSizeY)
+    local isXBound = targetPosX > rootPosX and targetPosX < (rootPosX + rectSizeX)
+    local isYBound = targetPosY > rootPosY and targetPosY < (rootPosY + rectSizeY)
     return isXBound and isYBound
 end
 
