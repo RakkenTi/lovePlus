da746b47a084a2fb990eacdb6ee7d9e26338e0ac
RakkenTi <martin3zmrk@gmail.com>
2025-04-05 14:31:46 -0400

    merged button.lua -> repo

---
+++ button.lua
@@ -0,0 +1,39 @@
+--// Modules
+local vector2 = require("src.shared.utils.vector2")
+local rect = require("src.modules.rect")
+local mouse = require("src.shared.utils.mouse")
+local run = require("src.shared.utils.run")
+
+--- A class for creating buttons
+--- A button is a rect wrapped with special events.
+--- @class buttonClass
+local button = {}
+button.__index = button
+
+--- @class button : buttonClass
+--- @param rect Rect
+--- Wraps a rectangle with special button behaviour.
+function button.new(rect)
+    local self = setmetatable({}, button)
+
+    self.callback = function() end
+    self.rect = rect
+    mouse.onRelease(function()
+        if (self.rect:isInside(vector2.new(love.mouse.getPosition()))) then
+            if (self.callback) then
+                self.callback()
+            end
+        end
+    end)
+
+    return self
+end
+
+--- Set the callback for the button-press action
+--- Note that a button press only accounts for a mouse being released within the buttons boundaries.
+--- @param callback function
+function button:setCallback(callback)
+    self.callback = callback
+end
+
+return button
--- utils.lua
+++ utils.lua
@@ -1,17 +1,17 @@
 --- Random utility methods for any purposes
 --- @class Util
-local util = {}
+local utils = {}
 
 --- Returns -1, 0, 1 depending on the sign of the number.
 --- @param num number
 --- @return number
-function util.getSignFromNum(num)
+function utils.getSignFromNum(num)
     if (num > 0) then return 1 elseif (num < 0) then return -1 else return 0 end
 end
 
 --- Returns the mouses position scaled relative to the screen as a tuple.
 --- @return number, number
-function util.getMouseScalePos()
+function utils.getMouseScalePos()
     local sx, sy = love.window.getMode()
     local x, y = love.mouse.getPosition()
     return x / sx, y / sy
@@ -19,7 +19,7 @@ end
 
 --- Get the position of the mouse as a number ranging from 0-1.
 --- @return number, number
-function util.getMouseScalePos()
+function utils.getMouseScalePos()
     local screenSizeX, screenSizeY = love.window.getMode()
     local mousePosX, mousePosY = love.mouse.getPosition()
     return mousePosX / screenSizeX, mousePosY / screenSizeY
@@ -30,7 +30,7 @@ end
 --- @param min number
 --- @param max number
 --- @return number
-function util.clamp(num, min, max)
+function utils.clamp(num, min, max)
     if (num > max) then
         return max
     end
@@ -46,13 +46,13 @@ end
 --- Useful for converting RGB to HST.
 --- @param colour number[]
 --- @return number[]
-function util.RGBtoHST(colour)
+function utils.RGBtoHST(colour)
     return {colour[1] / 255, colour[2] / 255, colour[3] / 255}
 end
 
 --- Lerp between a and b, using t
-function util.lerp(a, b, t)
+function utils.lerp(a, b, t)
     return a + (b - a) * t
 end
 
-return util
+return utils
