901df3f7dff48458d8cf5cce5c2c21a137a23db4
RakkenTi <martin3zmrk@gmail.com>
2025-04-06 15:14:52 -0400

    + ./window.lua init

---
+++ window.lua
@@ -0,0 +1,17 @@
+--- @class window
+local window = {}
+
+function window.init()
+
+end
+
+function window.update(dt)
+end
+
+function window.draw()
+end
+
+function window.exit()
+end
+
+return window
